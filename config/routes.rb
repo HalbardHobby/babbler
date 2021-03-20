3dc85db11d4a71605526bb8ee18575c339f3f7fc
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-19 21:10:58 -0500

    🚕 Routed babbles to index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :babbles
   post 'babbles/mark_as_read' => 'babbles#mark_as_read'
   devise_for :users
-  root 'home#index'
+  root 'babbles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
